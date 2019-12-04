d485ddbb8b929b96a7856b5be20a8a7b3c1668a5
mouWorks <mou.wang@g.ncu.edu.tw>
2019-12-04 22:39:08 +0800

    Add Swagger Doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+      .setTitle('Styles Api')
+      .setDescription('Api doc for Styles')
+      .setVersion('1.0')
+      // .addTag('styles')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
