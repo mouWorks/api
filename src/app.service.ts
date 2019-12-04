76cf6787a8a9432aa50c911a5b965f0ee8051462
mouWorks <mou.wang@g.ncu.edu.tw>
2019-12-04 21:09:33 +0800

    Try to push online

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,48 +9,6 @@ import { Style } from "./entity/Style";
 export class AppService {
 
   getHello(): string {
-    return 'Hello NestJS @3005 !';
+    return 'Hello NestJS @3005 001 !';
   }
-
-  // getStyles(): string {
-  //   return 'Listing all the desired Styles';
-  // }
-
-  // async delete(id): Promise<DeleteResult> {
-  //   return await this.StyleRepository.delete(id);
-  // }
-
-  async getStyleById(id): Promise<Object>{
-
-    const conn = await createConnection();
-
-    const styles = await conn.getRepository(Style).findOne({
-      id: id,
-      isDeleted: false
-    });
-
-    await conn.close();
-
-    return styles;
-  }
-
-  async createStyles() {
-
-    const style = new Style();
-
-    style.style = "blues";
-    style.desc = "布魯斯2";
-    style.descChinese = "Blues is a lovely dance";
-
-    const conn = await createConnection();
-
-    await conn.manager.save(style);
-
-    const users = await conn.manager.find(Style);
-
-    console.log(users);
-    return users;
-  }
-
-
 }
