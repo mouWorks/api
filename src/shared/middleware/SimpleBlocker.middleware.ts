e3a158265a9a689f5cb5fe4259dfca82914cd9f2
mouWorks <mou.wang@g.ncu.edu.tw>
2020-01-01 17:11:31 +0800

    finally this thing works now

--- src/shared/middleware/SimpleBlocker.middleware.ts
+++ src/shared/middleware/SimpleBlocker.middleware.ts
@@ -1,27 +1,21 @@
 import { EasyconfigService } from "nestjs-easyconfig";
-import {Injectable, NestMiddleware} from "@nestjs/common";
+import {Injectable, NestMiddleware, UnauthorizedException} from "@nestjs/common";
 
 @Injectable()
 export class SimpleBlocker implements NestMiddleware{
     constructor (private conf: EasyconfigService) {}
 
     use(req: Request, res: Response, next: () => void) {
-
         const magic_word = this.conf.get('MAGIC_WORD');
-
         if(req.body['magic_word'] == undefined){
-            console.log('You Did not say the magic word!');
-            // throw new UnauthorizedException();
+            // console.log('You Did not say the magic word!');
+            throw new UnauthorizedException('No magic word!');
         }else if(req.body['magic_word'] != magic_word){
-            console.log('Wrong Magic word!');
-            // throw new UnauthorizedException();
+            // console.log('Wrong Magic word!');
+            throw new UnauthorizedException('Wrong Magic Word!');
         }else{
-            console.log('Varified');
+            console.log('Verified POST Request');
             next();
         }
-
-        // console.log(req);
-        // console.log('我是簡單的Middleware: block unauthorized request');
-        // next();
     }
 }
