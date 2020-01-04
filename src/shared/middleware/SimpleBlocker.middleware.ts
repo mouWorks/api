462e14d6c1b9111a7c52e5cca9b14623258558f1
mouWorks <mou.wang@g.ncu.edu.tw>
2020-01-04 18:50:04 +0800

    Fix middleware

--- src/shared/middleware/SimpleBlocker.middleware.ts
+++ src/shared/middleware/SimpleBlocker.middleware.ts
@@ -1,6 +1,5 @@
 import { Injectable, NestMiddleware, UnauthorizedException} from '@nestjs/common';
 import { ConfigService } from "nestjs-dotenv";
-import * as dotenv from 'dotenv';
 
 // SimpleBlocker Middleware
 // Block - you need to provide the magic word
@@ -12,17 +11,15 @@ export class SimpleBlocker implements NestMiddleware{
     use(req: Request, res: Response, next: () => void) {
 
         const env = this.config.get('NODE_ENV');
-        if ((env !== undefined) && env === 'PROD'){
+        if ((env !== undefined) && env === 'local'){
             const magic_word = this.config.get('MAGIC_WORD');
             if(req.body['magic_word'] == undefined){
                 throw new UnauthorizedException('No magic word!');
             }else if(req.body['magic_word'] != magic_word){
                 throw new UnauthorizedException('Wrong Magic Word!');
-            }else{
-                console.log('Verified POST Request');
-                next();
             }
         }
+        //Just do it here.
         next();
     }
 }
